f5b422c1c7758bbf622cfe3384698a033cc5d807
edveloso <edveloso@gmail.com>
2022-10-31 16:29:05 -0300

    criacao do jpautil

--- src/main/java/br/edu/infnet/orm/modelo/persistencia/JpaUtil.java
+++ src/main/java/br/edu/infnet/orm/modelo/persistencia/JpaUtil.java
@@ -1,7 +1,18 @@
 package br.edu.infnet.orm.modelo.persistencia;
 
-public class JpaUtil {
-	
-	
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
+public class JpaUtil {
+
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+
+	public static void close() {
+		emf.close();
+	}
 }
